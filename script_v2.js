4eb3e52743bd9df16c4906f5af7aacb89859ec81
4ihtt <thierrydossantoscardoso@gmail.com>
2025-08-04 19:21:30 -0300

    Add files via upload

--- script_v2.js
+++ script_v2.js
@@ -1,6 +1,11 @@
-// Alerta ao clicar nos botões
+const clickSound = new Audio('https://freesound.org/data/previews/256/256113_3263906-lq.mp3');
+clickSound.volume = 0.2;
+
 document.querySelectorAll('.link-button').forEach(button => {
     button.addEventListener('click', () => {
+        clickSound.currentTime = 0;
+        clickSound.play();
+
         console.log(`Você clicou no link: ${button.textContent.trim()}`);
     });
 });
